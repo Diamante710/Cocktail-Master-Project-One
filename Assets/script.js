de44fa54d8348d30fa9a2dd42b3872cc62ac6302
Diamante710 <diamante710@icloud.com>
2023-02-06 21:00:29 -0700

    skeletons

---
+++ Assets/script.js
@@ -0,0 +1,42 @@
+// list of variables
+// const api key variable
+
+// need to append variables to parts of html
+
+// create eventlistener button for search Bar
+
+
+
+
+
+
+
+const apiKey = ''
+
+var findCoktail = function(event) {
+    var cocktailApi = ``
+    fetch (cocktailApi)
+    .then(function (response) {
+    console.log (response)
+    return Response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+        return data;
+    });
+}
+
+var findMocktail = function(event) {
+    var mocktailApi = ``
+    fetch (mocktailApi)
+    .then(function (response) {
+    console.log (response)
+    return Response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+        return data;
+    });
+}
+// create functions with above variables?
+
