5f80253caa07082a6fb120bd0b167ffbefabfe55
Diamante710 <diamante710@icloud.com>
2023-02-14 20:22:27 -0700

    script

--- Assets/script.js
+++ Assets/script.js
@@ -133,9 +133,6 @@ $(document).ready(function () {
   });
 });
 
-
-
-
 // fetch("https://icanhazdadjoke.com/", { headers: { "Accept": "application/json" } })
 //   .then(response => response.json())
 //   .then(data => console.log(data.joke))
